58fb93b3c916a3b0432c8bf3bb863c7a90c24ba4
Francsy <francsyludens@gmail.com>
2023-01-20 18:41:00 +0100

    add/función para búsqueda por comida favorita

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -29,6 +29,9 @@ const getUser = (req, res, next) => {
     else if (arrDB.some(user => user.address.country === search)) { 
         let usersByCountry = arrDB.filter(user => user.address.country === search);
         res.status(200).json(usersByCountry);
+    } else if (arrDB.some(user => user.favouritesFood.includes(search))){
+        let usersByFavFood = arrDB.filter(user => user.favouritesFood.includes(search))
+        res.status(200).json(usersByFavFood)
     } else {
         let error = new Error('User not found.')
         error.status = 404,
@@ -38,7 +41,7 @@ const getUser = (req, res, next) => {
 
 // En la función anterior: Otra opción para meter el req.params.searchUser(search) con '_' en lugar de espacios => user.address.country.split(' ').join('_') === search;
 
-const countUsers = (req, res, error) => {
+const countUsers = (req, res, next) => {
     try {
         res.status(200).json({
             total: getData().length
@@ -48,6 +51,10 @@ const countUsers = (req, res, error) => {
     }
 }
 
+
+const usersByVehicle = (req, res, next) => {
+    console.log(req.query)
+}
 // const getUsersByCountry = (req, res, error) => {
 //     let arrUsers = getData().filter(user => user.address.country === req.params.country)
 //     if (arrUsers) {
@@ -65,4 +72,5 @@ module.exports = {
     getAllUsers,
     getUser,
     countUsers,
+    usersByVehicle
 }
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -6,7 +6,10 @@ usersRouter.get('/', usersController.getAllUsers)
 
 usersRouter.get('/total', usersController.countUsers)
 
+usersRouter.get('/vehicles', usersController.usersByVehicle)
+
 usersRouter.get('/:searchUser', usersController.getUser)
+
 
 
 
