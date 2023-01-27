7755140dc60491f3f01962a6f741b374a9b66ce9
Francsy <francsyludens@gmail.com>
2023-01-27 09:44:34 +0100

    add/variable de entorno

--- controllers/dataWriterController.js
+++ controllers/dataWriterController.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
+require('dotenv').config()
 
 const path = require('path')
 const dbPath = path.join(__dirname, '..', 'db', '/users.json')
-const dataPath = path.join(__dirname, '..', '/tmp', '/users.json') || '/tmp/users.json'
+const dataPath = process.env.DATA_PATH || path.join(__dirname, '..', '/tmp', '/users.json')
 
 
 const dataWriter = (req,res) => {
